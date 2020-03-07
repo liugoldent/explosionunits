09206cdae423e93df974cf3df6612eae06aa6c44
a19025 <a19025@linktravel.com.tw>
2020-03-07 21:38:40 +0800

    first

---
+++ hello-next/pages/index.js
@@ -0,0 +1,25 @@
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
+    const h1Color = {
+        color: 'red'
+      };
+  return (
+    <Layout>
+      <h1 style={h1Color}>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}
